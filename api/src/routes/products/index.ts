d4fcf51a1c634646bdb3a35e27ebf5a1d52b30d8
Jean-Yves Barba <jybarba@gmail.com>
2024-10-12 00:01:22 +0100

    Products Router setup

---
+++ api/src/routes/products/index.ts
@@ -0,0 +1,19 @@
+import { Router } from "express";
+
+//products endpoints
+const router = Router();
+
+router.get('/', (req, res) => {
+    res.send('List of products');
+})
+
+router.get('/:id', (req, res) => {
+    console.log(req.params);
+    res.send('A product');
+})
+
+router.post('/', (req, res) => {
+    res.send('New product created');
+})
+
+export default router;
